f37c350e5df917917d9cf79570fd0993d65eda6e
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2019-04-15 11:04:21 -0600

    Hid the title bar and changed background to blue

--- app/src/main/java/jeff/game/wrenchgame/MainActivity.java
+++ app/src/main/java/jeff/game/wrenchgame/MainActivity.java
@@ -1,8 +1,10 @@
 package jeff.game.wrenchgame;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getSupportActionBar().hide();
 
+        LinearLayout root = findViewById(R.id.root);
+        root.setBackgroundColor(Color.BLUE);
     }
 }
